daafd196814eb95e4ca56dd7f001ef099bb3531c
VTCodeCraft <work.vishesh12.05@gmail.com>
2025-10-18 13:49:58 +0530

    user login , authuser middleware and redis setup

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,8 @@
 import userModel from '../models/user.model.js';
 import * as userService from '../services/user.services.js';
 import { validationResult } from 'express-validator';
+import redisClient  from '../services/redis.service.js';       
+
 
 export const createUserController = async (req, res) => {
     const errors = validationResult(req);
@@ -17,4 +19,48 @@ export const createUserController = async (req, res) => {
     } catch (error) {
         return res.status(500).json(error.message);
     }
+}
+
+export const loginUserController = async (req, res) => {
+         const errors = validationResult(req);
+         if (!errors.isEmpty()) {
+                  return res.status(400).json({ errors: errors.array() });
+         }
+         try {
+                  const { email, password } = req.body;
+
+                  const user = await userModel.findOne({ email}).select('+password'); // Explicitly select password field
+
+                  if (!user) {
+                           return  res.status(401).json({
+                                    errors:'Invalid Credentials'
+                           
+                           })
+                  }
+
+                  const isMatch = await user.isValidPassword(password);
+
+                  if (!isMatch) {
+                           return res.status(401).json({
+                                    errors:'Invalid Credentials'
+                           })
+                  }
+
+                  const token = await user.generateJWT();
+
+                  return res.status(200).json({ user, token });
+         }catch (error) {
+                  return res.status(400).json(error.message);          
+         }
+}
+
+export const profileController = async (req, res) => {
+
+         console.log(req.user);
+
+         res.status(200).json({
+                user: req.user  
+         })
+
+
 }
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,14 +1,23 @@
 import {Router} from 'express';
 import * as userController from '../controllers/user.controller.js'
 import { body } from 'express-validator';
+import * as authMiddleware from '../middleware/auth.middleware.js'
 
 const router = Router();
 
 router.post('/register',
          body('email').isEmail().withMessage('Enter a valid email address'),
-         body('password').isLength({ min: 3 }).withMessage('Password must be at least 6 characters long'),
-         userController.createUserController);
+         body('password').isLength({ min: 3 }).withMessage('Password must be at least 3 characters long'),
+         userController.createUserController
+);
 
+router.post('/login',
+         body('email').isEmail().withMessage('Enter a valid email address'),
+         body('password').isLength({ min: 3 }).withMessage('Password must be at least 3 characters long'),
+         userController.loginUserController           
+         ); 
+         
+router.get('/profile', authMiddleware.authUser , userController.profileController);
 
 
 
