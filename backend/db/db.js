5f32aeb5f1e7ac4836d835f4c141c8e0243f06a3
VTCodeCraft <work.vishesh12.05@gmail.com>
2025-10-18 11:56:33 +0530

    Initialcommit: Add backend files

---
+++ backend/db/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+
+function connect() {
+         mongoose.connect(process.env.MONGODB_URI)
+         .then(() => {
+           console.log("MongoDB connected successfully");
+         }).catch((err) => {
+           console.error("MongoDB connection error:", err);
+         });
+}
+
+export default connect;
